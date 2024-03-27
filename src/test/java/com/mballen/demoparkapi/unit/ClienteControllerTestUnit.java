95aa44f877e0cb7d0caf4962ae131256ee6e9e48
victor emanuel <victoremanuelvieria0@gmail.com>
2024-03-27 07:32:49 -0300

    finalizado teste unitario da Classe ClienteController

---
+++ src/test/java/com/mballen/demoparkapi/unit/ClienteControllerTestUnit.java
@@ -0,0 +1,147 @@
+package com.mballen.demoparkapi.unit;
+
+import com.mballen.demoparkapi.entity.Cliente;
+import com.mballen.demoparkapi.entity.Usuario;
+import com.mballen.demoparkapi.jwt.JwtUserDetails;
+import com.mballen.demoparkapi.repository.projection.ClienteProjection;
+import com.mballen.demoparkapi.service.ClienteService;
+import com.mballen.demoparkapi.service.UsuarioService;
+import com.mballen.demoparkapi.web.controller.ClienteController;
+import com.mballen.demoparkapi.web.dto.ClienteCreateDto;
+import com.mballen.demoparkapi.web.dto.ClienteResponseDto;
+import com.mballen.demoparkapi.web.dto.PageableDto;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.time.LocalDateTime;
+import java.util.Collections;
+
+@ExtendWith(MockitoExtension.class)
+public class ClienteControllerTestUnit {
+    private static final String USERNAME                = "usuarioTestUnitl@gmail.com";
+    private static final String PASSWORD                = "123456";
+
+    private static final Long ID = 1L;
+    private static final String NOME = "Usuario teste";
+    private static final String CPF = "25945427098";
+    private static final Usuario USUARIO = new Usuario();
+    private static final LocalDateTime DATA_CRIACAO = LocalDateTime.now();
+    private static final LocalDateTime DATA_MODIFICACAO = LocalDateTime.now();
+    private static final String CRIADO_POR = "useranonymus";
+    private static final String MODIFICADO_POR = "useranonymus";
+    @InjectMocks
+    private ClienteController clienteController;
+    @Mock
+    private ClienteService clienteService;
+    @Mock
+    private UsuarioService usuarioService;
+
+    private Cliente cliente;
+
+    private JwtUserDetails userDetails;
+    private ClienteCreateDto clienteCreateDto;
+    private Usuario usuario;
+    private Page<ClienteProjection> page;
+    @BeforeEach
+    void setUp() {
+        startCliente();
+        startUsuario();
+        startsUserDetails();
+    }
+    @Test
+    void whenCreateThenReturnClienteResponseEntity() {
+        Mockito.when(usuarioService.buscarPorId(Mockito.anyLong())).thenReturn(usuario);
+        ResponseEntity<ClienteResponseDto> response = clienteController.create(clienteCreateDto,userDetails);
+
+        Assertions.assertNotNull(response);
+        Assertions.assertEquals(response.getBody().getClass(), ClienteResponseDto.class);
+        Assertions.assertEquals(response.getStatusCode(), HttpStatus.CREATED);
+        Assertions.assertEquals(response.getClass(),ResponseEntity.class);
+        Assertions.assertEquals(response.getBody().getNome(),NOME);
+        Assertions.assertEquals(response.getBody().getCpf(),CPF);
+    }
+    @Test
+    void whenFindByIdThenReturnClienteResponseEntity(){
+        Mockito.when(clienteService.buscarPorId(Mockito.anyLong())).thenReturn(cliente);
+
+        ResponseEntity<ClienteResponseDto> response = clienteController.findById(ID);
+        Assertions.assertNotNull(response);
+        Assertions.assertEquals(response.getBody().getClass(), ClienteResponseDto.class);
+        Assertions.assertEquals(response.getStatusCode(), HttpStatus.OK);
+        Assertions.assertEquals(response.getClass(),ResponseEntity.class);
+        Assertions.assertEquals(response.getBody().getNome(),NOME);
+        Assertions.assertEquals(response.getBody().getCpf(),CPF);
+    }
+    @Test
+    void whenGetAllThenReturnResponseEntityPageableOfClientes(){
+        Mockito.when(clienteService.buscarTodos(Mockito.any())).thenReturn(page);
+
+        ResponseEntity<PageableDto> response = clienteController.getAll(PageRequest.of(0,10));
+
+        Assertions.assertEquals(response.getStatusCode(),HttpStatus.OK);
+        Assertions.assertEquals(response.getBody().getTotalElements(),1);
+        Assertions.assertEquals(response.getClass(),ResponseEntity.class);
+        Assertions.assertEquals(response.getBody().getClass(), PageableDto.class);
+
+    }
+    @Test
+    void whenGetDetalhesThenReturnClienteRespondeDto(){
+        Mockito.when(clienteService.buscarUsuarioPorId(Mockito.anyLong())).thenReturn(cliente);
+        ResponseEntity<ClienteResponseDto>response = clienteController.getDetalhes(userDetails);
+
+        Assertions.assertNotNull(response);
+        Assertions.assertEquals(response.getStatusCode(),HttpStatus.OK);
+        Assertions.assertEquals(response.getClass(),ResponseEntity.class);
+        Assertions.assertEquals(response.getBody().getClass(), ClienteResponseDto.class);
+        Assertions.assertEquals(response.getBody().getCpf(),CPF);
+        Assertions.assertEquals(response.getBody().getNome(),NOME);
+
+    }
+    void startCliente() {
+        this.cliente = new Cliente(ID, NOME, CPF, USUARIO, DATA_CRIACAO,
+                DATA_MODIFICACAO, CRIADO_POR, MODIFICADO_POR);
+
+        this.clienteCreateDto = new ClienteCreateDto(NOME,CPF);
+
+        ClienteProjection clienteProjection = new ClienteProjection() {
+            @Override
+            public Long getId() {
+                return ID;
+            }
+
+            @Override
+            public String getNome() {
+                return NOME;
+            }
+
+            @Override
+            public String getCpf() {
+                return CPF;
+            }
+        };
+        this.page = new PageImpl<>(Collections.singletonList(clienteProjection));
+
+    }
+
+    void startUsuario() {
+        this.usuario =
+                new Usuario
+                        (ID, USERNAME, PASSWORD, DATA_CRIACAO, DATA_MODIFICACAO
+                                , CRIADO_POR, MODIFICADO_POR, Usuario.Role.ROLE_CLIENTE);
+    }
+    void startsUserDetails(){
+        this.userDetails = new JwtUserDetails(usuario);
+    }
+}
